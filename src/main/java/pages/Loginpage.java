0ef9270feedab0d866d7cd084fdaee36c2b4b93b
Umamahesh835 <umamumr@gmail.com>
2026-07-26 21:57:21 +0530

    commit

---
+++ src/main/java/pages/Loginpage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class Loginpage extends Basepage{
+
+	public Loginpage(WebDriver driver) {
+		super(driver);
+		
+	}
+
+}
--- src/test/java/testcases/BaseTest.java
+++ src/test/java/testcases/BaseTest.java
@@ -1,5 +1,25 @@
 package testcases;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import base.DriverFactory;
+import utilities.ConfigReader;
+
 public class BaseTest {
+	
+	public WebDriver driver;
+	
+	@BeforeTest
+	public void setUp() {
+		driver = DriverFactory.initDriver(ConfigReader.getBrowser());
+		driver.get(ConfigReader.getBaseUrl());
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		DriverFactory.quitdriver();
+	}
 
 }
